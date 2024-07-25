b1926ac8066adfe1719d933f89da98d27255506e
NotFrenk <franciscoleonfelici@gmail.com>
2024-07-25 10:01:22 +0200

    server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var iPortaTcp = 4201;
 var sIpAddress = "127.0.0.1"
 app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress +
 ':' + iPortaTcp));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 //pagina di invio della form
@@ -15,10 +16,10 @@ app.get('/formRegistrazione', (req, res) => {
     });
 
 //pagina dinvio della forme 
-app.get('/sendfile',(req, res) => {
-    console.log('Mi hai chiesto la form di senf file.html');
-    res.sendFile('sendfile.htmk', { root: './htdoc'});
-});
+app.get('/sendFile', (req, res) => {
+	console.log("Mi hai chiesto la form di invio del file");
+	res.sendFile("sendfile.html", { root: './htdoc' });
+	});
 
 app.post('/mansendfile', (req, res) => {
     console.log(req.body.password)
@@ -30,7 +31,7 @@ app.post('/mansendfile', (req, res) => {
     }
 });
     
-vv
+
 //pagina di gestione dei dati della form se il metodo è GET
 app.get('/gestisciDatiForm', (req, res) => {
 console.log(req.query.fname);
